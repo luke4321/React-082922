2c31dc754344d52f39b3360b50b30b6a2d05fb51
lukeyzhong <lukeyzhong@gmail.com>
2022-08-31 16:34:01 -0500

    update

---
+++ JS/index.js
@@ -0,0 +1,60 @@
+// -----------------Day1---------------------------------
+
+// Javascript vs nodejs
+
+// primitive data
+
+// coercion
+
+// == vs ===
+
+// Var vs. let vs. const
+
+// Oop: object oriented programming in js
+
+// Encapsulation inheritance polymorphism abstraction
+
+// Constructor function 
+
+// Prototype chain 
+
+// -----------------Day2---------------------------------
+
+// Loop 
+
+// Array methods: myForEach myMap myFilter myReduce 
+
+// destrucure 
+
+// rest parameter vs spread operator 
+
+// object copy 
+
+// -----------------Day3---------------------------------
+
+// iife
+
+// closure
+
+// currying
+
+// this
+
+// call apply bind
+
+// -----------------Day4---------------------------------
+// XHR
+
+// Callback function callback hell
+
+// Promise
+
+// Myfetch 
+
+// -----------------Day5---------------------------------
+
+// Todolist 
+
+
+
+
